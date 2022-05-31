dad8ae78e3b10edbcf662c79244b03a20c8c6c8e
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-05-31 11:55:07 +0200

    **First Commit**

---
+++ apple.js
@@ -0,0 +1,6 @@
+var boxcostprice = document.getElementById("costPrice");
+var numberofappleinabox = document.getElementById("numberOfApples");
+var packetsize = document.getElementById("packetSize");
+var requiredprofit = document.getElementById("requiredProfit");
+
+var 
