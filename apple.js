ea0504a0798f7334fa763a2d465bb0956104a604
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-05-31 15:29:05 +0200

    **Forth Commit**

--- apple.js
+++ apple.js
@@ -1,43 +1,37 @@
-var boxcostprice = document.getElementById("costPrice")[0];
-var numberofappleinabox = document.getElementById("numberOfApples")[0];
-var packetsize = document.getElementById("packetSize")[0];
-var requiredprofit = document.getElementById("requiredProfit")[0];
+var boxcostprice = document.querySelector("#costPrice");
+var numberofappleinabox = document.querySelector("#numberOfApples");
+var packetsize = document.querySelector("#packetSize");
+var requiredprofit = document.querySelector("#requiredProfit");
 
-var numberofpackets = document.getElementById("numberOfPackets")[0];
-var costperapple = document.getElementById("costPricePerApple")[0];
-var costpriceperpacket = document.getElementById("costPricePerPacket")[0];
-var recommendedpacketprice = document.getElementById("recommendedpacketPrice")[0];
+var numberofpackets = document.querySelector("#numberOfPackets");
+var costperapple = document.querySelector("#costPricePerApple");
+var costpriceperpacket = document.querySelector("#costPricePerPacket");
+var recommendedpacketprice = document.querySelector("#recommendedPacketPrice");
 
-var button = document.getElementById("button")[0];
+var button = document.querySelector("#button");
 
 
 function calculateNumberOfPackets() {
-    var numberofpackets = numberofappleinabox.value / packetsize.value;
-    var numberofpackets1 = Math.ceil(numberofpackets);
+    var numberofpackets1 = Math.ceil(numberofappleinabox.value / packetsize.value);
     numberofpackets.innerHTML = numberofpackets1;
     return numberofpackets1;
 }
 
 function calculateCostPerApple() {
-    var costperapple = boxcostprice.value / numberofappleinabox.value;
-    var costperapple1 = Math.ceil(costperapple);
-    var costperapple2 = costperapple1 + (costperapple1 * requiredprofit.value);
-    costperapple.innerHTML = costperapple2;
-    return costperapple2;
+    var costperapple1 = Math.ceil(boxcostprice.value / numberofappleinabox.value);
+    costperapple.innerHTML = costperapple1;
+    return costperapple1;
 }
 
 function calculateCostPerPacket() {
     var costperpacket = calculateCostPerApple() * packetsize.value;
-    var costperpacket1 = Math.ceil(costperpacket);
-    var costperpacket2 = costperpacket1 + (costperpacket1 * requiredprofit.value);
-    costpriceperpacket.innerHTML = costperpacket2;
-    return costperpacket2;
+    costpriceperpacket.innerHTML = costperpacket;
+    return costperpacket;
 }
 
 function calculateRecommendedPacketPrice() {
-    var recommendedpacketprice = calculateCostPerPacket() * 1.2;
-    var recommendedpacketprice1 = Math.ceil(recommendedpacketprice);
-    var recommendedpacketprice2 = recommendedpacketprice1 + (recommendedpacketprice1 * requiredprofit.value);
+    var recommendedpacketprice1 = calculateCostPerApple() * packetsize.value;
+    var recommendedpacketprice2 = recommendedpacketprice1 + (recommendedpacketprice1 * requiredprofit.value / 100);
     recommendedpacketprice.innerHTML = recommendedpacketprice2;
     return recommendedpacketprice2;
 }
